f1ebd569793f640cc8cec1642a02454527cf1890
Diogo Malfatti <diimalfatti@gmail.com>
2023-07-15 10:59:59 -0300

    update

--- js/telaTreino.js
+++ js/telaTreino.js
@@ -1,5 +1,6 @@
 function voltar() {
-  window.location.href = "/boxnutrifisico/treinos.html";
+  window.location.href =
+    "https://diogomalfatti.github.io/boxnutrifisico/treinos.html";
 }
 // Obter dados do armazenamento local
 const dadosTreino = JSON.parse(localStorage.getItem("dadosTreino"));
